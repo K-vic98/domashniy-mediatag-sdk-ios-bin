0cc0e4ed5983871964c258e098738ed534c57315
maxim.mironov <maxim.mironov@cifrasoft.com>
2023-05-30 14:52:18 +0400

    checksum

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
       .binaryTarget(
           name: "MediatagSDK",
           url: "https://github.com/MEDIASCOPE-JSC/mediatag-sdk-ios-bin/archive/refs/tags/1.2.4.zip",
-          checksum: "4ef1f91a75140d1034318d7b1320f0ac386636167a22e71c7c0e69cdf1cfcb36"
+          checksum: "84c0671ef610d8dc3db42a40b1931104fc79cf6ac2922c1e364d98ca875b4f85"
       )
     ]
 )

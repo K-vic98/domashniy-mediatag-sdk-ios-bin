44c27d42d3d0631ec2d27b7177808c106684f925
maxim.mironov <maxim.mironov@cifrasoft.com>
2025-05-16 16:51:39 +0400

    v1.2.5

---
+++ ios-arm64/MediatagSDK.framework/Headers/MediatagSDK.h
@@ -0,0 +1,18 @@
+//
+//  MediatagSDK.h
+//  MediatagSDK
+//
+//  Created by Maksim Mironov on 17.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MediatagSDK.
+FOUNDATION_EXPORT double MediatagSDKVersionNumber;
+
+//! Project version string for MediatagSDK.
+FOUNDATION_EXPORT const unsigned char MediatagSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediatagSDK/PublicHeader.h>
+
+

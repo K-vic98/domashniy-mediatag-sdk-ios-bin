8a1347dbebe772575c10f47b3436102125e7619b
miromax21 <32737409+miromax21@users.noreply.github.com>
2025-06-04 13:07:21 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
       .binaryTarget(
           name: "MediatagSDK",
           url: "https://github.com/MEDIASCOPE-JSC/mediatag-sdk-ios-bin/archive/refs/tags/1.2.5.zip",
-          checksum: "0fb72253faaab60d5d80d2656b04c5c354e888167fddd7ee8ef6c9ac6069d9c5"
+          checksum: "f4445d2e57b7f01a09768e8e2392097e460d41dc5e7b1c8eb60a464dee292fa7"
       )
     ]
 )
